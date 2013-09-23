c5228939e9db0cbe258f1b8015bbceb9a4f118ca
Andrey Popp <8mayday@gmail.com>
2013-09-24 02:55:16 +0400

    Deps transform now ignores errors
    This is made not to throw an error on non-js files.
    Syntax check should be done later.

--- transforms/deps.js
+++ transforms/deps.js
@@ -4,7 +4,12 @@ var detective = require('detective')
 
 module.exports = function(mod, graph) {
   if (graph.noParse(mod.id)) return
-  var deps = detective(mod.source)
+
+  try {
+    var deps = detective(mod.source)
+  } catch(e) {
+    return
+  }
 
   return graph.resolveMany(deps, mod)
     .then(function(deps) { return {deps: deps} })
